cbd49221638bced0bae7965bacb0814790900895
Raj-Ram-007 <75797504+Raj-Ram-007@users.noreply.github.com>
2021-05-28 18:43:05 +0100

    Main

---
+++ Readme.txt
@@ -0,0 +1,37 @@
+npm install
+npm run dev
+
+
+---
+
+Get Emmit working 
+
+    Open Command Palette (Ctrl + Shift + P)
+
+    Open Settings (JSON)
+
+    Add to the end:
+
+    "emmet.triggerExpansionOnTab": true,
+    "emmet.includeLanguages": {
+        "javascript": "javascriptreact",
+        "vue-html": "html",
+        "razor": "html",
+        "plaintext": "jade"
+    }
+
+
+
+---
+
+Heroicons 
+
+        https://heroicons.com/
+        npm install @heroicons/react
+
+
+
+
+---
+
+    https://youtu.be/VA-UN_6oNG8?t=3038
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header>
+      {/*Top NAV  */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* Search bar        */}
+
+        <div className="hidden sm:flex rounded-md items-center flex-grow flex-shrink  cursor-pointer h-10 bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4 " />
+        </div>
+      </div>
+
+      {/*Bottom NAV  */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
